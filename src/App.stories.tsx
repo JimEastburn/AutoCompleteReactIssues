3906c0d64362f9a73f5804250891cc198a40e2c1
jim.eastburn <jim.eastburn@care.com>
2021-03-03 20:00:13 -0600

    changing files over to typescript

---
+++ src/App.stories.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+import { Story, Meta } from "@storybook/react/types-6-0";
+
+import App from "./App";
+
+export default {
+  title: "Pages/App",
+  component: App,
+} as Meta;
+
+const Template: Story = (args) => <App {...args} />;
+    
+export const MainStory = Template.bind({});
+MainStory.args = {
+  
+  /*👇 The args you need here will depend on your component */
+};
